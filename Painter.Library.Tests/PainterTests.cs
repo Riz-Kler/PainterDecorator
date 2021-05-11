aa81723d5fc0a794702a816624e883f2ad8b9722
Riz Kler <rizwan.kler@gmail.com>
2021-05-11 18:37:32 +0100

    Add project files.

---
+++ Painter.Library.Tests/PainterTests.cs
@@ -0,0 +1,114 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Painter.Library.Tests
+{
+    [TestFixture]
+    public class PainterTests
+    {
+        [Test]
+        public void Floor_Area__When11_Returns1()
+        {
+            //Arrange
+
+            double length = 1;
+            double width = 1;
+
+            //Act
+            double floorArea = PaintRoom.FloorArea(length, width);
+
+            //Asserts
+            Assert.AreEqual("1", floorArea);
+        }
+        [Test]
+        public void Floor_Area__When22_Returns4()
+        {
+            //Arrange
+
+            double length = 2;
+            double width = 2;
+
+            //Act
+            double floorArea = PaintRoom.FloorArea(length, width);
+
+            //Asserts
+            Assert.AreEqual("4", floorArea);
+        }
+
+        [Test]
+        public void Floor_Area__When00_Returns0()
+        {
+            //Arrange
+
+            double length = 0;
+            double width = 0;
+
+            //Act
+            double floorArea = PaintRoom.FloorArea(length, width);
+
+            //Asserts
+            Assert.AreEqual("0", floorArea);
+        }
+        [Test]
+        public void Floor_Area__WhenMinus15_Returns0()
+        {
+            //Arrange
+
+            double length = -1;
+            double width = 5;
+
+            //Act
+            double floorArea = PaintRoom.FloorArea(length, width);
+
+            //Asserts
+            Assert.AreEqual("-5", floorArea);
+        }
+
+        [Test]
+        public void Floor_Area__When5Minus1_Returns0()
+        {
+            //Arrange
+
+            double length = 5;
+            double width =-1;
+
+            //Act
+            double floorArea = PaintRoom.FloorArea(length, width);
+
+            //Asserts
+            Assert.AreEqual("-5", floorArea);
+        }
+
+        [Test]
+        public void Painted_Area_When000_Returns0()
+        {
+            //Arrange
+
+            double length = 0;
+            double width = 0;
+            double height = 0;
+
+            //Act
+            double paintedArea = PaintRoom.PaintedArea(length, width, height);
+
+            //Asserts
+            Assert.AreEqual("0", paintedArea);
+
+        }
+
+        public void Painted_Area_When111_Returns1()
+        {
+            //Arrange
+
+            double length = 1;
+            double width = 1;
+            double height = 1;
+
+            //Act
+            double paintedArea = PaintRoom.PaintedArea(length, width, height);
+
+            //Asserts
+            Assert.AreEqual("1", paintedArea);
+         }
+    }
+}
---
+++ Painter.Library/PaintRoom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Painter.Library
+{
+    public class PaintRoom
+    {
+        public static double FloorArea(double length, double width)
+        {
+
+            if(length<= 0 || width <=0)
+            {
+                return 0;
+            }
+
+            //calculate the area of the floor
+            double floorArea = width * length;
+
+            return floorArea;
+            
+        }
+
+        public static double PaintedArea(double length, double width, double height)
+        {
+            if (length <= 0 || width <= 0 || height <=0)
+            {
+                return 0;
+            }
+
+            //calculate the amount of paint required
+            double paintRequired = 2 * ((length * height) + (width + height));
+            return paintRequired;
+        }
+    }
+}
